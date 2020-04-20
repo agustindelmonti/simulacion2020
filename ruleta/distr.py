07da5c1be966a34585f2e2f72fe222d7b556ba47
Agustin <agus.delmonti@gmail.com>
2020-04-20 12:51:52 -0300

    Fix valores teoricos

--- ruleta/distr.py
+++ ruleta/distr.py
@@ -12,18 +12,20 @@ max = 37
 prob = 1 / (max - min)
 
 
-def distribution():
-    f, axes = plt.subplots(1, 1, figsize=(6, 3), sharex=True)
+def distribution(df):
+    fig, axes = plt.subplots(1, 1, figsize=(6, 3), sharex=True)
     sns.distplot(df, color="m", bins=37, label='frec. relativa')
 
     axes.axhline(prob, color='red', ls='--', alpha=0.5,
                  label=f"p = {format(prob, '.4f')}")
 
     axes.legend(loc='lower center')
+    axes.set_ylabel("frec. relativa")
     plt.tight_layout()
     fig.savefig(f"{out_folder}/distr.png")
 
 
 if __name__ == "__main__":
     df = pd.read_csv("./results/out.txt", header=None)
+    sns.set(style="darkgrid", palette="muted", color_codes=True)
     distribution(df)
