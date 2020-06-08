5a20a3e6b0736a66535a63fea8d90bbd1273b1a0
ignacioc97 <63249123+ignacioc97@users.noreply.github.com>
2020-06-08 15:40:42 -0300

    Add files via upload

---
+++ 2.2.distribuciones/Exponencial.py
@@ -0,0 +1,40 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
+import pandas as pd
+import math
+
+# LCG Implementation
+
+def lcg(a, c, m, initial_seed):
+    seed = initial_seed
+    while True:
+        rand = (a * seed + c) % m
+        seed = rand
+        yield rand
+
+def random_sample(seed):
+    sample = []
+    varAux = lcg(1103590199, 419329, (2 ** 32), seed)
+    for i in range(100):
+        observation = next(varAux) / (2 ** 32)
+        sample.append(observation)
+
+    return sample
+
+def exponencial(media, seed):
+    sample = []
+    array = (random_sample(seed))
+    for r in range(len(array)):
+        x = - media * math.log(array[r])
+        sample.append(x)
+    return sample
+
+#Main
+seed = 4294966661
+media = 9
+data = []
+data = (exponencial(media, seed))
+
+for j in range(len(data)):
+    print(data[j])
